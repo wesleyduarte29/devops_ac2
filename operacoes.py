c40abf806247508e852ffb017eb70bc451425a74
wesleyduarte29 <61299308+wesleyduarte29@users.noreply.github.com>
2020-04-24 02:02:05 -0300

    operacoes_python

---
+++ operacoes.py
@@ -0,0 +1,18 @@
+class Operacoes():
+    def soma(self, valores):
+        val = 0
+        for v in valores:
+            val = val + v
+        return val
+
+    def subtracao(self, valores):
+        val = 0
+        for v in valores:
+            val = val - v
+        return val
+
+    def mult(self, valores):
+        val = 2
+        for v in valores:
+            val = val * v
+        return val
